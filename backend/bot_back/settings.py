4e555b16840052845e75931b31c6b343e5103746
Robert Levchenko <ro.le.00@mail.ru>
2023-06-17 06:30:00 +0300

    infra refactoring

--- backend/bot_back/settings.py
+++ backend/bot_back/settings.py
@@ -133,7 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/backend_static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+
+MEDIA_URL = '/backend_media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
