11bfed6480cec42fe710f81d7751be033e03624e
Robert Levchenko <ro.le.00@mail.ru>
2023-07-03 18:06:09 +0300

    commence to implement serializers.py

--- backend/quiz/serializers.py
+++ backend/quiz/serializers.py
@@ -9,3 +9,35 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ("task_index", "question", "answer", "image")
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Answer."""
+
+    class Meta:
+        model = Answer
+        fields = ("task_index", "question", "answer", "image")
+
+
+class HintSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Hint."""
+
+    class Meta:
+        model = Hint
+        fields = ("task_index", "question", "answer", "image")
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Tag."""
+
+    class Meta:
+        model = Task
+        fields = ("task_index", "question", "answer", "image")
+
+
+class StatSerializer(serializers.ModelSerializer):
+    """Сериализатор модели Statistic."""
+
+    class Meta:
+        model = Statistic
+        fields = ("task_index", "question", "answer", "image")
